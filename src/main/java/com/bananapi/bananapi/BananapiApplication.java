3f7d6520adb1e830b85041bafd200f154f31bf31
emmanuel-zbz <ezacben103@g.educaand.es>
2026-03-22 02:37:48 +0100

    Initial Commit

---
+++ src/main/java/com/bananapi/bananapi/BananapiApplication.java
@@ -0,0 +1,13 @@
+package com.bananapi.bananapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BananapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bananapi/bananapi/BananapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bananapi.bananapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BananapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
