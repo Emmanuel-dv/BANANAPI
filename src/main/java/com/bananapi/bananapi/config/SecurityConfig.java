32aa820c1895fbee90b1baa27e731dca04453e27
Emmanuel-dv <emmanuel.zacariasbenitez@gmail.com>
2026-05-01 20:27:30 +0200

    Añadido controllerAdvice para el manejo global de expceciones

--- src/main/java/com/bananapi/bananapi/config/SecurityConfig.java
+++ src/main/java/com/bananapi/bananapi/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.bananapi.bananapi.config;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
