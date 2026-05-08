d0ec5776b90b64028618e7eb95ee046e86a660cf
Emmanuel-dv <emmanuel.zacariasbenitez@gmail.com>
2026-05-08 15:51:29 +0200

    finalizacion implementacion JWT y refactorizacion de codigo SecurityConfig, UserService y Nuevo DTO respuesta

--- src/main/java/com/bananapi/bananapi/domain/User.java
+++ src/main/java/com/bananapi/bananapi/domain/User.java
@@ -2,6 +2,11 @@ package com.bananapi.bananapi.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -10,7 +15,7 @@ import lombok.*;
 @Getter
 @Setter
 @Builder
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +31,28 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
--- src/main/java/com/bananapi/bananapi/security/JwtAuthFilter.java
+++ src/main/java/com/bananapi/bananapi/security/JwtAuthFilter.java
@@ -1,5 +1,6 @@
 package com.bananapi.bananapi.security;
 
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,34 +22,69 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
 
+
+    /**
+     * Metodo para procesar JWT
+     * <p>
+     * Esta clase intercepta las peticiones antes que la aplicación para poder validarlas en el contexto de seguridad de Spring, es decir, que estén autenticados.
+     * Cada peticion que se haga, ahora se sabrá quien la ha hecho (como resumen).
+     *
+     * @param request     la petición HTTP que se nos envía desde cliente con todas sus cabeceras etc.
+     * @param response    la respuesta que mandamos al siguiente filtro
+     * @param filterChain La interfaz que usamos para dejar dejar pasar al siguiente filtro de la aplicacion
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        //Aqui ingresa como: "Bearer " etc......
+        /*
+            Cada cabecera de una petición HTTP tiene un nombre y diferente información contenida por esa cabecera. Se puede investigar los nombres por convención atribuidos.
+            Cogemos toda la información del Header de autenticación (que lleva el nombre de Authorization)
+            Esto nos devuelve el String del token que sería: "Bearer " y a continuación el token codificado.
+         */
         String authHeader = request.getHeader("Authorization");
 
-        if (authHeader == null || authHeader.startsWith("Bearer ")){
+        /*
+            Verificamos que haya algo dentro de la cabecera de autentificación y que lo que haya empiece con el formato estándar de JWT
+            En caso de que alguna sea errónea, detenemos la ejecución de este filtro y escalamos la petición en otro lugar en vez de cortar el flujo aquí.
+            Esto haría que falle luego con un código http de 401
+         */
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
 
-
+        //Cogemos todo el string del token codificado
         String token = authHeader.substring(7);
 
-        if (jwtService.verifyToken(token, jwtService.getUsernameDecoded(token))) {
+
+        String nombreUsuario = null;
+
+        /*
+            Probamos que el nombre de usuario esté dentro del token, si falla porque viene null o cualquier cosa ingresa al catch y hace que pase al siguiente filtro
+            Esto evita que mas adelante por el contexto vacío de un fallo 500 puesto que se corta el flujo. De este modo da un 400 algo (volver a mirar)
+         */
+        try {
+            nombreUsuario = jwtService.getUsernameDecoded(token);
+        } catch (JwtException e) {
             filterChain.doFilter(request, response);
             return;
         }
 
-
+        //Creamos un contexto de seguridad vacío porque queremos asignarle nosotros uno propio
         SecurityContext contexto = SecurityContextHolder.createEmptyContext();
 
-        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(jwtService.getUsernameDecoded(token), null, Collections.emptyList());
+        //Esto es algo un pooc antiguo el nombre, pero sigue funcionando puesto que a esta peticion HTTP le asignamos un nombre, que es el del usuario
+        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(nombreUsuario, null, Collections.emptyList());
 
+        //Rellenamos la caja vacía del contexto con la identidad que hemos creado.
         contexto.setAuthentication(authRequest);
 
+        //Asignamos el contexto al hilo de ejecucion para que sepa que a una petición HTTP debe asignarle un propietario, y este será identificado mediante su nombre de usuario.
         SecurityContextHolder.setContext(contexto);
 
+        //Pasamos a la siguiente capa
         filterChain.doFilter(request, response);
 
     }
